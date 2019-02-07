556da510fdab8cbc9c78e48a82f67a14f50579d3
mentix02 <manan.yadav02@gmail.com>
2019-02-07 21:38:08 +0530

    finished v1.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as rf:
 
 setuptools.setup(
     name='wordpy',
-    version='1.1.1',
+    version='1.1.2',
     url='https://github.com/mentix02/wordpy',
     license='MIT',
     author='manan',
